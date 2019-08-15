f3d15b9eed19ae6180eb443fe92725510be45820
girishbin <girish.bhat06@gmail.com>
2019-08-16 03:49:31 +0530

    Update pssh_collect.py

--- pssh_collect.py
+++ pssh_collect.py
@@ -1,33 +1,48 @@
-#!/usr/bin/python
-import os
-import sys
+"""
+Code that goes along with the Airflow located at:
+http://airflow.readthedocs.org/en/latest/tutorial.html
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
 
-try:
-   sys.argv[1]
-except IndexError:
-   print "Please give the command as argument in \" \" quotes"
-   sys.exit()
-   
-if os.popen("id -u").read().rstrip("\n")!=0:
-   os.system("rm -fr ./test")
-else:
-   print "Danger you are root !!"
 
-CSI="\x1B["   # for coloring  
-command = "pssh -O StrictHostKeyChecking=no -o test -h '%s'  -l girishb '%s'"  %(sys.argv[2],sys.argv[1])
-state = os.system(command)
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 6, 1),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
 
-if state!=0:
-   print "ERROR running command\n"
-   sys.exit()
+dag = DAG("tutorial", default_args=default_args, schedule_interval=timedelta(1))
 
-dirlist = os.listdir("./test")
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 
-for files in dirlist:
-   filepath ="./test/%s" %(files)
-   fh = open(filepath)
-   #CSI="\x1B["
-   print CSI+"31;40m" + "%s" %(files) + CSI + "0m" 
-   lines = fh.read()
-   print lines,
-   print "\n"
+t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
+
+templated_command = """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7)}}"
+        echo "{{ params.my_param }}"
+    {% endfor %}
+"""
+
+t3 = BashOperator(
+    task_id="templated",
+    bash_command=templated_command,
+    params={"my_param": "Parameter I passed in"},
+    dag=dag,
+)
+
+t2.set_upstream(t1)
+t3.set_upstream(t1)
